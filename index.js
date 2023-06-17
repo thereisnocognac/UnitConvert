ce535f2e40d6226cd5a575483d1b60c9fa7dba0b
thereisnocognac <yollovesxyra@gmail.com>
2023-06-17 12:28:56 -0400

    initialized js functions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const lengthC = document.querySelector('#LengthConv');
 const volumeC = document.querySelector('#VolumeConv');
 const massC = document.querySelector('#MassConv');
-let inputNum = document.querySelector('.number-container');
+let inputNum = document.querySelector('#number-container');
 let convBtn = document.querySelector('#conv-btn');
 
+convBtn.addEventListener('click', function convert() {
+    
+    let toBe = inputNum.value;
+    let lengthChange = toBe * 3.281
+    
+    lengthC.innerHTML += `${lengthChange.toFixed(4)} is HERE!`
+    volumeC.innerHTML += `${toBe} is HERE!`
+    massC.innerHTML += `${toBe} is HERE!`
+})
