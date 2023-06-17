a90247cc2dc1c8f0bd7a32e32cad2465804d2e86
thereisnocognac <yollovesxyra@gmail.com>
2023-06-17 11:44:12 -0400

    added js vars

---
+++ index.js
@@ -0,0 +1,6 @@
+const lengthC = document.querySelector('#LengthConv');
+const volumeC = document.querySelector('#VolumeConv');
+const massC = document.querySelector('#MassConv');
+let inputNum = document.querySelector('.number-container');
+let convBtn = document.querySelector('#conv-btn');
+
